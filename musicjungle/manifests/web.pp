e40c2066967a1bdec60f1ae4330a277eff9a8bb3
Nogueira <fernando.nogueira@saude.ce.gov.br>
2019-04-11 16:29:10 -0300

    Add tomcat restart service

--- musicjungle/manifests/web.pp
+++ musicjungle/manifests/web.pp
@@ -7,4 +7,20 @@ package {
 package { 
   'unzip': ensure => installed, require => Exec["apt-update"]
 }
-
+service { 
+  "tomcat7":
+    ensure => running,
+    enable => true,
+    hasstatus => true,
+    hasrestart => true,
+    require => Package["tomcat7"]    
+}
+file { 
+  "/var/lib/tomcat7/webapps/vraptor-musicjungle.war":
+    source => "/vagrant/manifests/vraptor-musicjungle.war",
+    owner => "tomcat7",
+    group => "tomcat7",
+    mode => 0644,
+    require => Package["tomcat7"],
+    notify => Service["tomcat7"]
+}
